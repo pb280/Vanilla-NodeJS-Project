c19de82c61659e93e3591c1dd2d908890e90c60a
pb280 <123048899+pb280@users.noreply.github.com>
2023-04-21 19:18:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,109 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf')
+const flash = require('connect-flash')
+const multer = require('multer')
+
+const errorController = require('./controllers/error')
+const User = require('./models/user')
+
+const MONGODB_URI =
+  'mongodb+srv://prathamesh:cLxcgxkqCaQwIgJ9@cluster0.p7tfc4h.mongodb.net/shop'
+
+const app = express()
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
+const csrfProtection = csrf()
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+const authRoutes = require('./routes/auth')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+)
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+)
+app.use(csrfProtection)
+app.use(flash())
+app.use((req, res, next) => {
+  app.locals.isAuthenticated = req.session.isLoggedIn
+  app.locals.csrfToken = req.csrfToken()
+  next()
+})
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next()
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next()
+      }
+      req.user = user
+      next()
+    })
+    .catch(err => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+})
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+app.use(authRoutes)
+app.get('/500', errorController.get500)
+app.use(errorController.get404)
+app.use((error, req, res, next) => {
+  console.log(error)
+  res.redirect('/500')
+})
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(4000)
+  })
+  .catch(err => {
+    console.log(err)
+  })
